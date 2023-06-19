51e0806f0b27fe5ebee4b7116c6b79670a09b45b
kuro7andmona <129634798+kuro7andmona@users.noreply.github.com>
2023-06-19 09:50:26 +0000

    Add devise_model create 1_no migrate

---
+++ app/models/customer.rb
@@ -0,0 +1,18 @@
+class Customer < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  #has_many :cart_items
+  #has_many :orders
+  #has_many :addresses
+
+ validates :last_name, presence: true
+ validates :last_name_kana, format: { with: /\A[\p{katakana}\u{30fc}]+\z/, message: 'カタカナで入力してください。'}
+ validates :first_name, presence: true
+ validates :first_name_kana, format: { with: /\A[\p{katakana}\u{30fc}]+\z/, message: 'カタカナで入力してください。'}
+ validates :postcode, format: { with: /\A\d{3}[-]\d{4}\z|\A\d{3}[-]\d{2}\z|\A\d{3}\z|\A\d{5}\z|\A\d{7}\z/ }
+ validates :address, presence: true
+ validates :phone_number, format: { with: /\A\d{10}\z|\A\d{11}\z/ }
+end
