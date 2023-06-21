811895b592f0c1ef12b7361344e47089422c6afd
EC2 Default User <ec2-user@ip-172-31-26-149.ap-northeast-1.compute.internal>
2023-06-21 02:52:22 +0000

    ADD model 3

--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -1,7 +1,7 @@
 class CartItem < ApplicationRecord
-  has_many :order_details
-  belongs_to :item
-  belongs_to :customer
+  #has_many :order_details
+  #belongs_to :item
+  #belongs_to :customer
   def subtotal
     item.with_tax_price * amount
 　end
--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -1,6 +1,6 @@
 class OrderDetail < ApplicationRecord
   enum status_method: {not_started: 0,pending: 1,in_progress: 2,completed: 3,}
-  belongs_to :item
-  belongs_to :cart_item
-  belongs_to :order
+  #belongs_to :item
+  #belongs_to :cart_item
+  #belongs_to :order
 end
