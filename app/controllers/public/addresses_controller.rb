2dd897acfda2741d4c8e5cd0f4684cd38a7e7881
DMMWEBCAMPGitHub <130573254+DMMWEBCAMPGitHub@users.noreply.github.com>
2023-06-22 05:15:14 +0000

    Add public_address_1

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -1,20 +1,29 @@
 class Public::AddressesController < ApplicationController
   def index
+    @ = current_user
+    @addresses = Address.all
   end
 
   def edit
+    @address = Address.find(params[:id])
   end
   
   def create
-    
+    @address = Address.new(addresse_params)
+    @address.save
+    redirect_to addresse_path(@address.id)
   end
   
   def update
-    
+    @address = Address.find(params[:id])
+    @address.update(address_params)
+    redirect_to address_path(@address.id)
   end
   
   def destroy
-    
+    @address = Address.find(params[:id])
+    @address.destroy
+    redirect_to address_path
   end
   
 end
