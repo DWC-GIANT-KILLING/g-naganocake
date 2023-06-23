600e5b6f127b80b51e017d051811a31cbd3990ab
kuro7andmona <129634798+kuro7andmona@users.noreply.github.com>
2023-06-23 13:00:30 +0000

    Add view 2

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,5 +6,14 @@ class Item < ApplicationRecord
   def with_tax_price
     (price * 1.1).floor
   end
+
+  #商品画像
+  #def get_item_image
+    #unless item_image.attached?
+      #file_path = Rails.root.join('app/assets/images/no_image.jpg')
+      #item_image.attach(io: File.open(file_path), filename: 'no_image.jpg', content_type: 'image/jpeg')
+    #end
+    #item_image.variant(resize_to_limit: [100, 200]).processed
+  #end
 end
 
