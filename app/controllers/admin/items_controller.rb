b498e5cc3ec2886cc59ee6191e443bf4cb86e88f
kuro7andmona <129634798+kuro7andmona@users.noreply.github.com>
2023-06-23 09:41:13 +0000

    Add

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,12 +1,16 @@
 class Admin::ItemsController < ApplicationController
   def index
+    @items = Item.all
   end
 
   def new
+    @items = Item.new
   end
 
   def create
-    
+    @item = Item.new(item_params)
+    @item.save
+    redirect_to admin_item_path(@item.id)
   end
 
   def show
@@ -14,8 +18,14 @@ class Admin::ItemsController < ApplicationController
 
   def edit
   end
-  
+
   def update
-    
+
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:genre_id, :name, :unit_price, :detail, :is_selling)
   end
 end
