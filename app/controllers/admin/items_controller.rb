37a7533031b5bfa6855f767937c52e36b3b63f15
kuro7andmona <129634798+kuro7andmona@users.noreply.github.com>
2023-06-24 10:27:24 +0000

    Add image

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,32 +1,50 @@
 class Admin::ItemsController < ApplicationController
   def index
     @items = Item.all
+
   end
 
   def new
-    @items = Item.new
+    @item = Item.new
   end
 
   def create
     @item = Item.new(item_params)
-    @item.save
-    redirect_to admin_item_path(@item.id)
+    if @item.save
+     redirect_to admin_item_path(@item)
+   else
+     render :new
+    end
   end
 
   def show
-    @item = Item.find(params[:])
+    @item = Item.find(params[:id])
   end
 
   def edit
+    @item = Item.find(params[:id])
   end
 
   def update
-
+      @item = Item.find(params[:id])
+    if @item.update(item_params)
+       redirect_to admin_item_path
+    else
+      render :edit
+    end
   end
+
+  def destroy
+    item = Item.find(params[:id])
+    item.destroy
+    redirect_to admin_items_path
+  end
+
+
 
   private
 
   def item_params
-    params.require(:item).permit(:genre_id, :name, :unit_price, :detail, :is_selling)
+    params.require(:item).permit(:id, :genre_id, :name, :unit_price, :detail, :is_selling,:image)
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -8,12 +8,12 @@ class Item < ApplicationRecord
   end
 
   #商品画像
-  #def get_item_image
-    #unless item_image.attached?
-      #file_path = Rails.root.join('app/assets/images/no_image.jpg')
-      #item_image.attach(io: File.open(file_path), filename: 'no_image.jpg', content_type: 'image/jpeg')
-    #end
-    #item_image.variant(resize_to_limit: [100, 200]).processed
-  #end
+  def get_item_image(width, height)
+    unless image.attached?
+      file_path = Rails.root.join('app/assets/images/no_image.jpg')
+      image.attach(io: File.open(file_path), filename: 'no_image.jpg', content_type: 'image/jpeg')
+    end
+     image.variant(resize_to_limit: [width, height]).processed
+  end
 end
 
