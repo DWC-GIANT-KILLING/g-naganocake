82ebe588b0b02b48ab48f0f4cf379a000e22ebd5
HaradaMaria <132239927+HaradaMaria@users.noreply.github.com>
2023-06-25 06:20:01 +0000

    Fix Admin/item 2

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -29,7 +29,7 @@ class Admin::ItemsController < ApplicationController
   def update
       @item = Item.find(params[:id])
       if @item.update(item_params)
-         #flash[:notice] = "登録商品を編集しました"
+         flash[:notice] = "登録商品を編集しました"
          redirect_to admin_item_path(@item)
       else
         render :edit
