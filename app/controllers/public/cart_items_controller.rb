17e739592aaf2d5ec8b4ca13867c2b882f2fb573
HaradaMaria <132239927+HaradaMaria@users.noreply.github.com>
2023-06-25 16:23:20 +0000

    Add cart 3

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -4,8 +4,10 @@ class Public::CartItemsController < ApplicationController
   end
   
   def update
-    @cart_item = Cart_item.find(params[:id])
-    if @cart_item.update(cart_item_params)
+    @in_cart = CartItem.find(params[:id])
+    @in_cart.item_id = @in_cart.item_id.to_i
+    @in_cart.piece = @in_cart.piece.to_i
+    if @in_cart.update(params[:id])
       flash[:notice] = "変更を保存しました"
       redirect_to cart_items_path(current_customer_id)
     else
@@ -15,9 +17,9 @@ class Public::CartItemsController < ApplicationController
   end
   
   def destroy
-    Cart_item.find(params[:id]).destroy
+    CartItem.find(params[:id]).destroy
     flash[:notice] = "商品を削除しました"
-    redirect_to cart_items_path(params[:current_customer_id])
+    redirect_to cart_items_path(current_customer)
   end
   
   def destroy_all
@@ -27,30 +29,29 @@ class Public::CartItemsController < ApplicationController
   end
   
   def create
+    @cart_item = CartItem.new(cart_item_params)
     @customer = current_customer
-    @item = Item.find
-    if @customer.cart_item.fid_by(item_id: @item)
-      @cart_item = Cart_item.new(cart_params)
-       in_cart = @customer.cart_item.fid(item_id: @item)
-       @cart_item.piece = in_cart.piece.to_i + @cart_item.piece.to_i
-       if@cart_item.update(cart_item_params)
-         flash[:notice] = "既存の商品に追加しました"
-         redirect to cart_items_path(current_customer_id)
-       else
-         flash[:notice] ="商品の追加に失敗しました"
-         render item_path(@item)
-       end
-    else
-      @cart_item = Cart_item.new(cart_params)
+    @item = @cart_item.item_id 
+    #if CartItem.find_by(item_id: @item)
+    #  @in_cart = CartItem.find_by(item_id: @item)
+    #  @in_cart.piece = @in_cart.piece.to_i + @cart_item.piece.to_i
+    #  if@in_cart.update(params[:id])
+    #    flash[:notice] = "既存の商品に追加しました"
+    #    redirect_to cart_items_path(current_customer_id)
+    #  else
+    #    flash[:notice] ="商品の追加に失敗しました"
+    #    render item_path(@item)
+    #  end
+    #else
       @cart_item.customer_id = current_customer.id
       if @cart_item.save
         flash[:notice] = "商品を追加しました"
-        redirect to cart_items_path(current_customer_id)
+        redirect_to cart_items_path(@customer)
       else
         flash[:notice] ="商品の追加に失敗しました"
-        render item_path(@item)
+        render template: 'public/items/show'
       end
-    end
+    #end
   end
   
   
@@ -60,4 +61,11 @@ class Public::CartItemsController < ApplicationController
     params.require(:cart_item).permit(:item_id, :piece)
   end
 
+  #def params_int(params)
+    #params do |id,piece|
+      #if integer_string?(value)
+        #params[:id] = piece.to_i
+      #end
+    #end
+  #end
 end
