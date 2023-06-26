f701c19558ac84e18be3fe6d1a9173d51e60f462
HaradaMaria <132239927+HaradaMaria@users.noreply.github.com>
2023-06-26 07:22:09 +0000

    Finish public/cart fix

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -8,11 +8,9 @@ class Public::CartItemsController < ApplicationController
   
   def update
     @in_cart = CartItem.find(params[:id])
-    @in_cart.item_id = @in_cart.item_id.to_i
-    @in_cart.piece = @in_cart.piece.to_i
-    if @in_cart.update(params[:id])
+    if @in_cart.update(cart_item_params)
       flash[:notice] = "変更を保存しました"
-      redirect_to cart_items_path(current_customer_id)
+      redirect_to cart_items_path(current_customer.id)
     else
       flash[:notice] = "変更を保存できませんでした"
       render cart_items_path(current_customer_id)
@@ -32,43 +30,37 @@ class Public::CartItemsController < ApplicationController
   end
   
   def create
-    @cart_item = CartItem.new(cart_item_params)
-    @customer = current_customer
-    @item = @cart_item.item_id 
-    #if CartItem.find_by(item_id: @item)
-    #  @in_cart = CartItem.find_by(item_id: @item)
-    #  @in_cart.piece = @in_cart.piece.to_i + @cart_item.piece.to_i
-    #  if@in_cart.update(params[:id])
-    #    flash[:notice] = "既存の商品に追加しました"
-    #    redirect_to cart_items_path(current_customer_id)
-    #  else
-    #    flash[:notice] ="商品の追加に失敗しました"
-    #    render item_path(@item)
-    #  end
-    #else
+      @cart_item = CartItem.new(cart_item_params)
+      @customer = current_customer
+      @item = @cart_item.item_id 
+    if CartItem.find_by(item_id: @item)
+      @in_cart = CartItem.find_by(item_id: @item)
+      new_piece = @in_cart.piece.to_i + @cart_item.piece.to_i
+      if@in_cart.update(piece: new_piece)
+        flash[:notice] = "既存の商品に追加しました"
+        redirect_to cart_items_path(current_customer.id)
+      else
+        flash[:notice] ="商品の追加に失敗しました"
+        render 'public/items/show'
+      end
+    else
       @cart_item.customer_id = current_customer.id
       if @cart_item.save
         flash[:notice] = "商品を追加しました"
         redirect_to cart_items_path(@customer)
       else
         flash[:notice] ="商品の追加に失敗しました"
-        render template: 'public/items/show'
+        @item = Item.find(params[:cart_item][:item_id])
+        render 'public/items/show'
       end
-    #end
+    end
   end
   
   
   private
   
   def cart_item_params
-    params.require(:cart_item).permit(:item_id, :piece)
+    params.require(:cart_item).permit(:id, :item_id, :piece)
   end
 
-  #def params_int(params)
-    #params do |id,piece|
-      #if integer_string?(value)
-        #params[:id] = piece.to_i
-      #end
-    #end
-  #end
 end
