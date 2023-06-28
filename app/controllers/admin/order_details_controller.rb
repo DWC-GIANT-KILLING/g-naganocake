e60f3ec00b0ad70a32a6467ddabb96a432d3ecc1
EC2 Default User <ec2-user@ip-172-31-26-149.ap-northeast-1.compute.internal>
2023-06-28 10:27:37 +0000

    fix status 2

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -2,9 +2,22 @@ class Admin::OrderDetailsController < ApplicationController
   before_action :authenticate_admin!
 
   def update
-   @order_details = OrderDetail.find(params[:id])
-   @order_details.update(order_detail_params)
-   redirect_to admin_order_path( @order_details.order)
+    @order_detail = OrderDetail.find(params[:id])
+    @order = @order_detail.order
+    @order_detail.update(order_detail_params)
+    if @order_detail.status == "in_progress"
+      @order.update(status: "production")
+    end
+
+    if @order_detail.status == "completed"
+      # やりたいこと: すべての商品の製作が完了しているか確認する
+
+      if @order.order_details.count == @order.order_details.where(status: 'completed').count
+        @order.update(status: "packing")
+      end
+    end
+
+    redirect_to admin_order_path( @order_detail.order)
   end
 
   private
